8cf7be9cbd735860e15f250cf22fcece1b45cbec
bekir inan <bekir.inan@ogr.dpu.edu.tr>
2019-05-08 06:03:12 +0300

    Some problem fixed.

--- projeA/Form1.Designer.cs
+++ projeA/Form1.Designer.cs
@@ -28,49 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pBox_urunler = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.resim = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pBox_urunler)).BeginInit();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resim)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pBox_urunler
+            // tabControl1
             // 
-            this.pBox_urunler.Location = new System.Drawing.Point(12, 12);
-            this.pBox_urunler.Name = "pBox_urunler";
-            this.pBox_urunler.Size = new System.Drawing.Size(408, 417);
-            this.pBox_urunler.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pBox_urunler.TabIndex = 0;
-            this.pBox_urunler.TabStop = false;
-            this.pBox_urunler.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pBox_urunler_MouseClick);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(771, 520);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.resim);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(763, 494);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Resim";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(459, 30);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Resim Yükle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // resim
+            // 
+            this.resim.Location = new System.Drawing.Point(53, 30);
+            this.resim.Name = "resim";
+            this.resim.Size = new System.Drawing.Size(378, 434);
+            this.resim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.resim.TabIndex = 10;
+            this.resim.TabStop = false;
+            this.resim.MouseClick += new System.Windows.Forms.MouseEventHandler(this.resim_MouseClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(763, 494);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mesaj";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(378, 434);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(546, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 23);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Mesaj Al";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(546, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Mesaj Gönder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(627, 357);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Port Kapat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(631, 60);
+            this.button1.Location = new System.Drawing.Point(546, 357);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Port aç";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(524, 283);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 49);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(524, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 239);
+            this.textBox1.TabIndex = 17;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(540, 30);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 23);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Resim Kaydet";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(595, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Port Statüs";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(773, 523);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pBox_urunler);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pBox_urunler)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resim)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pBox_urunler;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox resim;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- projeA/Form1.cs
+++ projeA/Form1.cs
@@ -1,58 +1,308 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace projeA
 {
+
     public partial class Form1 : Form
     {
+        string resimYolu;
+        string resimAdi;
+  
+        private SerialPort sp = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+       
+
+
+
+        private int mx , my;
+        private Graphics g;
+        private Pen _pencil2 = new Pen(Color.Green, 2);
+        private Pen _pencil = new Pen(Color.Red,2);
+
+
         public Form1()
         {
             InitializeComponent();
         }
-
         protected override void WndProc(ref Message m)
         {
             if (m.Msg == 0x219)
             {
-                pBox_urunler.Image = Image.FromFile("D:\\Downloads\\eia-tia-561-db25.jpg");
+
+                pictureBox1.Image = Image.FromFile("D:\\Downloads\\abc.jpg");
             }
 
             base.WndProc(ref m);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            sp.Open();
+            
+
+
+            label1.Text = "Port Açık";
+            MessageBox.Show("Port Açıldı");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                sp.Close();
+             
+                label1.Text = "Port Kapalı";
+                MessageBox.Show("Port Kapatıldı");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+
+                sp.Write(textBox1.Text);
+
+            }
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            int counter = 0;
+            string line,line2;
+
+           // while (true)
+           // {
+
+
+
+
+                try
+                {
+
+                    string message = sp.ReadLine();
+                    textBox2.Text = message;
+                    sp.ReadTimeout = 500;
+
+
+                }
+                catch (TimeoutException)
+                {
+
+                }
+
+           // }
+
+
+            StreamReader file = new StreamReader("F:/Visual Studio 2017/Projects/projeA/xKoordinatlar.gsr");
+            StreamReader file2 = new StreamReader("F:/Visual Studio 2017/Projects/projeA/yKoordinatlar.gsr");
+
+            
+              
+           
+            while ((line = file.ReadLine()) != null)
+            {
+                
+                if (line.Contains(textBox2.Text))
+                {
+                    line2 = file2.ReadLine();
+
+                    if (line.Contains("=1"))
+                    {
+                        line = file.ReadLine();
+                        line2 = file2.ReadLine();
+                        if (line.Contains("0") || line.Contains("1") || line.Contains("2")
+                        || line.Contains("3") || line.Contains("4") || line.Contains("5")
+                        || line.Contains("6") || line.Contains("7") || line.Contains("8") || line.Contains("9"))
+                        {
+                            g = pictureBox1.CreateGraphics();
+
+                            g.DrawRectangle(_pencil2, Convert.ToInt32(line)-40, Convert.ToInt32(line2)-40, 95, 105);
+                        }
+                    }
+                    else if (line.Contains("=0"))
+                    {
+                        line = file.ReadLine();
+                        line2 = file2.ReadLine();
+                        if (line.Contains("0") || line.Contains("1") || line.Contains("2")
+                        || line.Contains("3") || line.Contains("4") || line.Contains("5")
+                        || line.Contains("6") || line.Contains("7") || line.Contains("8") || line.Contains("9"))
+                        {
+                            g = pictureBox1.CreateGraphics();
+
+                            g.DrawRectangle(_pencil, Convert.ToInt32(line)-40, Convert.ToInt32(line2)-40, 95, 105);
+                        }
+                    }
+                    
+
+               
+
+                counter++;
+                }
+            }
+            file.Close();
+            file2.Close();
+
+
+            //Console.WriteLine(buffer.ToString());
+
+
+            /*while ((line = file.ReadLine()) != null)
+            {
+                List<string> deneme = new List<string>();
+                deneme.Add(line);
+
+               
+
+                if (line.Contains(textBox2.Text))
+                {
+                    g = pictureBox1.CreateGraphics();
+
+                    g.DrawRectangle(_pencil, , my - 40, 95, 105);
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine("Naber Ortak2");
+                }
+                
+
+                counter++;
+            }
+
+            file.Close();
+
+            Console.ReadLine();*/
+
+
+
+
+
 
         }
 
        
 
-        private void pBox_urunler_MouseClick(object sender, MouseEventArgs e)
+        private void Button5_Click(object sender, EventArgs e)
         {
-            Graphics g;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG" +
+            "|All files(*.*)|*.*";
+            dialog.CheckFileExists = true;
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
 
-            g = pBox_urunler.CreateGraphics();
+                resimYolu = dialog.FileName;
+                resimAdi = dialog.SafeFileName;
+                resim.ImageLocation = resimYolu;
 
-            Pen pencil=new Pen(Color.Red,2);
-           
-            g.DrawEllipse(pencil,e.X-5,e.Y-5,10,16);
-
-           
-            StreamWriter sw = File.AppendText("H:/Visual Studio 2017/Projects/projeA/kayitlar.txt");
-            sw.WriteLine("x: "+e.X+"y: "+e.Y +"\n");
-            sw.Close(); 
-
-            
+            }
         }
 
+        private void Button6_Click(object sender, EventArgs e)
+        {
+
+
+            
+
+            g = resim.CreateGraphics();
+
+            g.DrawRectangle(_pencil, mx - 40, my - 40, 95, 105);
+
+            var jpeg = new JpegMetadataAdapter(resimYolu);
+
+            string koordinatBilgisi = "";
+
+            
+                koordinatBilgisi = "Pin  X : " + mx + " Y : " + my + "\n";
+            
+            jpeg.Metadata.Comment = koordinatBilgisi;
+            jpeg.Metadata.Title = "Koordinatlar \n";
+            // jpeg.Save();            // Resmin kendisine kayıt etme istenirse yapılabilir.
+
+            string gsrYol = @"D:\Downloads\TezOdev" + resimAdi + " GsrDosyası.gsr";     //kaydedeceğimiz GSR Dosyasının yolunu belirle
+
+            jpeg.SaveAs(gsrYol);     // GSR Dosyası olarak kaydet
+            
+            MessageBox.Show("GSR Dosyası Başarıyla Kaydedildi.");
+
+        }
+
+        public class JpegMetadataAdapter
+        {
+            private readonly string path;
+            private BitmapFrame frame;
+            public readonly BitmapMetadata Metadata;
+
+            public JpegMetadataAdapter(string path)
+            {
+                this.path = path;
+                frame = getBitmapFrame(path);
+                Metadata = (BitmapMetadata)frame.Metadata.Clone();
+            }
+
+            public void Save()
+            {
+                SaveAs(path);
+            }
+
+            public void SaveAs(string path)
+            {
+                JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(frame, frame.Thumbnail, Metadata, frame.ColorContexts));
+                using (Stream stream = File.Open(path, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    encoder.Save(stream);
+                }
+            }
+
+            private BitmapFrame getBitmapFrame(string path)
+            {
+                BitmapDecoder decoder = null;
+                using (Stream stream = File.Open(path, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    decoder = new JpegBitmapDecoder(stream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
+                }
+                return decoder.Frames[0];
+            }
+        }
+
+        private void resim_MouseClick(object sender, MouseEventArgs e)
+        {
+            mx = e.X - 5;
+            my = e.Y - 5;
+            
+           
+
+            g = resim.CreateGraphics();
+
+            g.DrawRectangle(_pencil, mx - 40, my - 40, 95, 105);
+
+            StreamWriter sw = File.AppendText("F:/Visual Studio 2017/Projects/projeA/abc.jpg");
+            StreamWriter sw2 = File.AppendText("F:/Visual Studio 2017/Projects/projeA/xKoordinatlar.gsr");
+            StreamWriter sw3 = File.AppendText("F:/Visual Studio 2017/Projects/projeA/yKoordinatlar.gsr");
+            sw.WriteLine("x: "+" "+mx+" " +"y: "+" "+my);
+            sw2.WriteLine(mx);
+            sw3.WriteLine(my);
+
+            sw.Close();
+            sw2.Close();
+            sw3.Close();
+            
+
+
+        }
 
     }
 }
